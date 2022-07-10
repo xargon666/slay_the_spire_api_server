570044404dc1f95e27d6f5f5cdc87c1bce74b5cd
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-10 16:29:08 +0100

    adjust file structure, add ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_cors import CORS 
 import os
+from models.Relics import Relics
 
 print("hello from app.py")
 
@@ -11,10 +12,17 @@ app = Flask(__name__)
 
 # Connect app to local db in heroku
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
 
+#SQLALCHEMY_TRACK_MODIFICATIONS
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# Routes ******************************************************************************************
 @app.route("/favicon.ico")
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
@@ -25,43 +33,12 @@ def catch_all(path):
     print(path)
     return render_template("index.html")
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-# postgres://jremcexhoaamgj:781f3245c24665d1e930c6a310d6dd87e934937aa8f5942f120a1aa41114ec52@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/df67jta2kdc8ba
-
-
-
-# Setup Model for db
-class RelicModel(db.Model):
-    __tablename__ = 'relics'
-
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String())
-    rarity = db.Column(db.String())
-    class_specific = db.Column(db.String())
-    description = db.Column(db.String())
-    flavor_text = db.Column(db.String())
-    conditions_for_spawning = db.Column(db.String())
-
-    def __init__(self,name,rarity,class_specific,description,flavor_text,conditions_for_spawning):
-        self.name = name
-        self.rarity = rarity
-        self.class_specific = class_specific
-        self.description = description
-        self.flavor_text = flavor_text
-        self.conditions_for_spawning = conditions_for_spawning
-    
-    def __repr__(self):
-        return f"<Relic {self.name}"
-    
-@app.route('/relics', methods=['POST','GET','DELETE'])
+@app.route('/relics', methods=['POST','GET'])
 def handle_relics():
     match request.method:
         case 'POST':
             data = request.get_json()
-            new_relic = RelicModel(
+            new_relic = Relics(
                 name=data['name'],
                 rarity=data['rarity'],
                 class_specific=data['class_specific'],
@@ -74,7 +51,7 @@ def handle_relics():
             return {"message":f"New relic {new_relic.name} has been added to the db"}
 
         case 'GET':
-            relics = RelicModel.query.all()
+            relics = Relics.query.all()
             results = [
             {
                 "id": relic.id,
@@ -86,11 +63,41 @@ def handle_relics():
                 "conditions_for_spawning": relic.conditions_for_spawning
             } for relic in relics ]
             return {"count": len(results), "relics": results}
-
-        case 'DELETE':
-            return
         case _:
-            return "Unsupported request"
+            return "You have made an unsupported request"
+
+@app.route('/relics/<id>', methods=['PUT','GET'])
+def handle_relics_id(id):
+    match request.method:
+        case 'PUT':
+            data = request.get_json()
+            new_relic = Relics(
+                name=data['name'],
+                rarity=data['rarity'],
+                class_specific=data['class_specific'],
+                description=data['description'],
+                flavor_text=data['flavor_text'],
+                conditions_for_spawning=data['conditions_for_spawning']
+            )
+            db.session.add(new_relic)
+            db.sessions.commit()
+            return {"message":f"New relic {new_relic.name} has been added to the db"}
+
+        case 'GET':
+            relics = Relics.query.all()
+            results = [
+            {
+                "id": relic.id,
+                "name": relic.name,
+                "rarity": relic.rarity,
+                "class_specific": relic.class_specific,
+                "description": relic.description,
+                "flavor_text": relic.flavor_text,
+                "conditions_for_spawning": relic.conditions_for_spawning
+            } for relic in relics ]
+            return {"count": len(results), "relics": results}
+        case _:
+            return "You have made an unsupported request"
 
 
 # # # run db setup and seed
