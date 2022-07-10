fe873006736bb8d95db76d9cc105a664ffe13b9f
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-10 11:33:59 +0100

    add basic routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify, request, render_template, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, text
 from flask_migrate import Migrate 
@@ -24,25 +24,83 @@ if __name__ == "__main__":
 
     app.run(debug=True)
 
-# @app.route('/product', methods=['GET'])
-# def get_relics():
-#     all_relics = "" # db.query.all()
-
-
 # Connect app to local db in heroku
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-ac = app.config['SQLALCHEMY_DATABASE_URI']
-print(ac)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# # run db setup and seed
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+# Setup Model for db
+# ('name','rarity','class-specific','description','flavor-text','conditions-for-spawning')
+class RelicModel(db.Model):
+    __tablename__ = 'cars'
 
-with engine.connect() as con:
-    with open("./migrations/1_setup.sql") as file:
-        query = text(file.read())
-        con.execute(query)
-    with open("./migrations/2_seed.sql") as file:
-        query = text(file.read())
-        con.execute(query)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String())
+    rarity = db.Column(db.String())
+    class_specific = db.Column(db.String())
+    description = db.Column(db.String())
+    flavor_text = db.Column(db.String())
+    conditions_for_spawning = db.Column(db.String())
+
+    def __init__(self,name,rarity,class_specific,description,flavor_text,conditions_for_spawning):
+        self.name = name
+        self.rarity = rarity
+        self.class_specific = class_specific
+        self.description = description
+        self.flavor_text = flavor_text
+        self.conditions_for_spawning = conditions_for_spawning
+    
+    def __repr__(self):
+        return f"<Relic {self.name}"
+    
+@app.route('/relics', methods=['POST','GET','DELETE'])
+def handle_relics():
+    match request.method:
+        case 'POST':
+            data = request.get_json()
+            new_relic = RelicModel(
+                name=data['name'],
+                rarity=data['rarity'],
+                class_specific=data['class_specific'],
+                description=data['description'],
+                flavor_text=data['flavor_text'],
+                conditions_for_spawning=data['conditions_for_spawning']
+            )
+            db.session.add(new_relic)
+            db.sessions.commit()
+            return {"message":f"New relic {new_relic.name} has been added to the db"}
+
+        case 'GET':
+            relics = RelicModel.query.all()
+            results = [
+            {
+                "id": relic.id,
+                "name": relic.name,
+                "rarity": relic.rarity,
+                "class_specific": relic.class_specific,
+                "description": relic.description,
+                "flavor_text": relic.flavor_text,
+                "conditions_for_spawning": relic.conditions_for_spawning
+            } for relic in relics ]
+            return {"count": len(results), "relics": results}
+
+        case 'DELETE':
+            return
+        case _:
+            return "Unsupported request"
+
+
+# # # run db setup and seed
+# engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+# with engine.connect() as con:
+#     with open("./migrations/1_setup.sql") as file:
+#         query = text(file.read())
+#         con.execute(query)
+#     with open("./migrations/2_seed.sql") as file:
+#         query = text(file.read())
+#         con.execute(query)
+
+# @app.route('/product', methods=['GET'])
+# def get_relics():
+#     all_relics = "" # db.query.all()
