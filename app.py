824d7876c7b36b2064d6e832a5da2e2ec4fd8098
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-10 10:32:45 +0100

    comment out code to get heroku working!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_from_directory
-from flask_sqlalchemy import SQLAlchemy, create_engine, text
+from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine, text
 from flask_migrate import Migrate # ???
 from flask_cors import CORS # for routes?
 import os
@@ -7,31 +8,31 @@ import os
 print("hello from app.py")
 
 app = Flask(__name__)
-
-# Connect app to local db in heroku
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL').replace("://", "ql://", 1)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 CORS(app)
 
-# run db setup and seed
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+# Connect app to local db in heroku
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+# ac = app.config['SQLALCHEMY_DATABASE_URI']
+# print(ac)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
-with engine.connect() as con:
-    with open("./migrations/1_setup.sql") as file:
-        query = text(file.read())
-        con.execute(query)
-    with open("./migrations/seed.sql") as file:
-        query = text(file.read())
-        con.execute(query)
+# # run db setup and seed
+# engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    print(path)
-    return render_template("index.html")
+# with engine.connect() as con:
+#     with open("./migrations/1_setup.sql") as file:
+#         query = text(file.read())
+#         con.execute(query)
+#     with open("./migrations/2_seed.sql") as file:
+#         query = text(file.read())
+#         con.execute(query)
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     print(path)
+#     return render_template("index.html")
 
 
 @app.route("/favicon.ico")
